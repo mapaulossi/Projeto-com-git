de4b6cfa6efea9fea43c5b50dc1807acf34d1713
Matheus Paulossi <mt_paulossi@hotmail.com>
2019-06-11 11:10:11 -0300

    Projeto iniciado com git

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Programa_git
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
